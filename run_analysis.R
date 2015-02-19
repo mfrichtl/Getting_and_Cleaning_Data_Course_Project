dc8ee62100fc70f7b90fab835844f55980b65713
mfrichtl <frichtlm@gmail.com>
2015-02-19 18:36:53 -0500

    Update run_analysis.R
    Fixed the dataset to include all mean and STD columns from the original dataset.

--- run_analysis.R
+++ run_analysis.R
@@ -3,12 +3,12 @@ library(data.table)
 
 ## Build the training data set
 ## Check to see if it exists first to save time
+names <- as.vector((read.table("./features.txt"))[[2]])
 if (!exists("training_data")) {
   table1 <- tbl_df(read.table("./train/subject_train.txt",
                               col.names = "subject"))
   table2 <- tbl_df(read.table("./train/y_train.txt", col.names = "label"))
-  table3 <- tbl_df(read.table("./train/X_train.txt",
-                              col.names = 1:561))
+  table3 <- tbl_df(read.table("./train/X_train.txt", col.names = names))
 
   training_data <- tbl_df(bind_cols(table1, table2, table3))
 }
@@ -18,8 +18,7 @@ if (!exists("testing_data")) {
   table1 <- tbl_df(read.table("./test/subject_test.txt",
                               col.names = "subject"))
   table2 <- tbl_df(read.table("./test/y_test.txt", col.names = "label"))
-  table3 <- tbl_df(read.table("./test/X_test.txt",
-                              col.names = 1:561))
+  table3 <- tbl_df(read.table("./test/X_test.txt", col.names = names))
 
   testing_data <- tbl_df(bind_cols(table1, table2, table3))
 }
@@ -35,8 +34,7 @@ if (!exists("merged")) {
 ## Extract the means and standard deviations for each data set
 if (!exists("extracted")) {
   extracted <- tbl_df(merged %>%
-    select(subject, label, X1:X6) %>%
-    rename(MeanX = X1, MeanY = X2, MeanZ = X3, StdX = X4, StdY = X5, StdZ = X6)
+    select(subject, label, contains("mean"), contains("std"))
   )
 }
 
@@ -49,19 +47,13 @@ activity_labels = read.table("./activity_labels.txt",
 data <- tbl_df(extracted)
 data <- tbl_df(data %>%
   merge(activity_labels, all = TRUE) %>%
-  merge(activity_labels, all = TRUE) %>%
   select(-label)
 )
 
+## Sort and summarize the data by subject and activity.
 sorted_data <- data %>%
   group_by(activity, subject) %>%
-  summarize(
-    meanX = mean(MeanX),
-    meanY = mean(MeanY),
-    meanZ = mean(MeanZ),
-    stdX = mean(StdX),
-    stdY = mean(StdY),
-    stdZ = mean(StdZ)
-  )
+  summarise_each(funs(mean))
 
-write.table(sorted_data, "./sorted_data.txt", sep = ",", row.names = FALSE)
+## Save the tidy data set as a text file.
+write.table(sorted_data, "./sorted_data.txt", row.names = FALSE)
